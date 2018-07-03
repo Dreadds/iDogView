2610404cd54af02979f42d0fa216b9f0a9450695
Dreadds <kevinatc10@gmail.com>
2018-07-03 01:17:32 -0500

    initial commit

---
+++ iDogView/ViewControllers/AboutDogsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutDogsViewController.swift
+//  iDogView
+//
+//  Created by Kevin Tito on 7/3/18.
+//  Copyright © 2018 Kevin Tito. All rights reserved.
+//
+
+import UIKit
+
+class AboutDogsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
